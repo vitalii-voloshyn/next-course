4e33fb0834622a55f12cffc5c6ee5f5933559326
Vitalii <mack@Vitaliis-MacBook-Pro.local>
2020-08-30 15:14:05 +0100

    Created simple routing

---
+++ pages/about/index.js
@@ -0,0 +1,38 @@
+import {useRouter} from 'next/router'
+import Head from 'next/head'
+
+export default function About() {
+  const router = useRouter()
+  return (
+  <>
+  <Head>
+    <title>About</title>
+    <meta name="keywords" content="ssr, react, redux" />
+    <meta charSe="utf-8" />
+  </Head>
+  <h1>About page</h1>
+  <div>
+  <button 
+  style={styles.leftButton}
+  onClick={() => router.push('/')} >
+     Go back to home page
+    </button>
+  <button 
+  style={styles.rightButton}
+  onClick={() => router.push('/posts')}>Go to posts</button>
+  </div>
+  </>
+  )
+}
+
+const styles = {
+  leftButton: {
+    marginRight: '20px',
+    padding: '5px 10px',
+    borderRadius: '5px'
+  },
+  rightButton: {
+    padding: '5px 10px',
+    borderRadius: '5px'
+  }
+}
---
+++ pages/posts.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+
+export default function Posts() {
+  return (
+   <>
+   <Head>
+     <title>Posts</title>
+     <meta name="keywords" content="next, react, redux" />
+   </Head>
+    <h1>Posts page</h1>
+   </>
+  )
+}
+// Routing
+// /posts
+// /posts/10
