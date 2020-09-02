f6e82aaed665952f1b598c9200d25cd7de27588b
Vitalii <mack@Vitaliis-MacBook-Pro.local>
2020-09-02 20:43:58 +0100

    Created dinamicly changing post and added loading with server delay

--- pages/post/[id].js
+++ pages/post/[id].js
@@ -1,12 +1,63 @@
-import { useRouter } from 'next/router'
+import {useState, useEffect} from 'react'
+import {useRouter} from 'next/router'
+import Link from 'next/link'
 import MainLayout from '../../components/MainLayout'
+import styles from './id.module.css'
 
-export default function Post() {
-  const router = useRouter()
-  console.log(router)
+export default function Post({post: serverPost}) {
+  const {query} = useRouter()
+  const [post, setPost] = useState(serverPost)
+  useEffect(() => {
+   async function load() {
+     const response = await fetch(`http://localhost:4200/posts/${query.id}`)
+     const data = await response.json()
+     setPost(data)
+   }
+   if (!serverPost) {
+     load()
+   }
+  }, [])
+
+if (!post) {
   return (
-    <MainLayout title={'Post'} >
-      <h1>Post {router.query.id}</h1>
+    <MainLayout>
+      <p>Loading ...</p>
     </MainLayout>
   )
 }
+  return (
+    <MainLayout title={'Post'} >
+    <div className={styles.post}>
+      <h1>
+        {post.title}
+      </h1>
+      <hr />
+      <p>{post.body}</p>
+      <Link href={`/posts`}><a>Go back to posts</a></Link>
+    </div>
+    </MainLayout>
+  )
+}
+
+
+Post.getInitialProps = async ({query, req}) => {
+  if (!req) {
+    return {post: null}
+  }
+  const response = await fetch(`http://localhost:4200/posts/${query.id}`)
+  const post = await response.json()
+  return {
+    post
+ }
+}
+      
+      // refactoring:
+      // Отрабатывает только на сервере
+      // export  async function getServerSideProps({query, req}) {
+      //   const response = await fetch(`http://localhost:4200/posts/${query.id}`)
+      //   const post = await response.json()
+      //   return {
+      //     props: {post}
+      //   }
+      // }
+      
--- pages/posts.js
+++ pages/posts.js
@@ -2,8 +2,28 @@ import React, {useEffect, useState} from 'react'
 import Link from 'next/link'
 import MainLayout from '../components/MainLayout'
 
-export default function Posts({posts}) { 
+export default function Posts({posts: serverPosts}) { 
+   const [posts, setPosts] = useState(serverPosts)
 
+   useEffect(() => {
+     async function load() {
+       const response = await fetch('http://localhost:4200/posts')
+       const data = await response.json()
+       setPosts(data)
+     }
+     if (!serverPosts) {
+        load()
+     }
+   })
+
+   if (!posts) {
+    return (
+      <MainLayout>
+        <p>Loading ...</p>
+      </MainLayout>
+    )
+  }
+// =============================
   // useEffect(() => {
   //   async function load() {
   //    const response = await fetch(`http://localhost:4200/posts`)
@@ -13,14 +33,11 @@ export default function Posts({posts}) {
   //   load()
   // }, [])
   // в SSR асинхронное получение данных не индексируется роботами, так как данные не отрисовуются сразу. 
-
+//===================================
   return (
    <MainLayout title={'Posts'} >
     <h1>Posts page</h1>
-    {/* <pre>
-      {JSON.stringify(posts)}
-    </pre> */}
-
+  
     <ul>
       {posts.map(post => (
         <li key={post.id}>
@@ -34,7 +51,12 @@ export default function Posts({posts}) {
   )
 }
 // Определяем статический метод который будет выполнятся на сервере getInitialProps встроеная функция в Next
-Posts.getInitialProps = async () => {
+Posts.getInitialProps = async ({req}) => {
+  if (!req) {
+    return {
+      posts: null
+    }
+  }
   const response = await fetch(`http://localhost:4200/posts`)
   const posts = await response.json()
   return {
