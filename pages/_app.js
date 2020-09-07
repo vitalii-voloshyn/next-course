21bf73e480ae246e9db5e81ea6a720ebc3a22e97
Vitalii <mack@Vitaliis-MacBook-Pro.local>
2020-09-07 20:25:13 +0100

    removed comments

--- pages/_app.js
+++ pages/_app.js
@@ -4,13 +4,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
     <Component {...pageProps} />
-   {/* <style jsx global>{`
-   body {
-     font-family: 'Roboto', sans-serif;
-     margin: 0;
-     padding: 0;
-   } 
-   `}</style> */}
     </>
   ) 
 }
